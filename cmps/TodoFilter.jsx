f98027db20d0cbe047f044d88d18356462809535
Ofir-Roz <ofir19923@gmail.com>
2024-12-25 18:23:17 +0200

    Refactor TodoFilter to use Redux for filter state management and add status filter options

--- cmps/TodoFilter.jsx
+++ cmps/TodoFilter.jsx
@@ -1,12 +1,17 @@
+import { SET_FILTER_BY } from "./../store/reducers/todo.reducer.js";
+
+const { useDispatch, useSelector } = ReactRedux
 const { useState, useEffect } = React
 
-export function TodoFilter({ filterBy, onSetFilterBy }) {
+export function TodoFilter() {
+    const dispatch = useDispatch()
 
-    const [filterByToEdit, setFilterByToEdit] = useState({...filterBy})
+    const filterBy = useSelector((state) => state.todoModule.filterBy)
+    const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
 
     useEffect(() => {
         // Notify parent
-        onSetFilterBy(filterByToEdit)
+        dispatch({ type: SET_FILTER_BY, filterBy: filterByToEdit})
     }, [filterByToEdit])
 
     function handleChange({ target }) {
@@ -35,7 +40,7 @@ export function TodoFilter({ filterBy, onSetFilterBy }) {
         onSetFilterBy(filterByToEdit)
     }
 
-    const { txt, importance } = filterByToEdit
+    const { txt, importance, isDone } = filterByToEdit
     return (
         <section className="todo-filter">
             <h2>Filter Todos</h2>
@@ -43,10 +48,18 @@ export function TodoFilter({ filterBy, onSetFilterBy }) {
                 <input value={txt} onChange={handleChange}
                     type="search" placeholder="By Txt" id="txt" name="txt"
                 />
-                <label htmlFor="importance">Importance: </label>
+                <label htmlFor="importance"> Importance: </label>
                 <input value={importance} onChange={handleChange}
                     type="number" placeholder="By Importance" id="importance" name="importance"
                 />
+
+                <label htmlFor="isDone"> Status: </label>
+                <select value={isDone} onChange={handleChange} name="isDone" id="isDone">
+                    <option value="" disabled>Select an option</option>
+                    <option value="">All</option>
+                    <option value="true">Not Done</option>
+                    <option value="false">Done</option>
+                </select>
 
                 <button hidden>Set Filter</button>
             </form>
