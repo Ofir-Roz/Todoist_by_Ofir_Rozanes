998a32c994ba4d7b179d9475c641c56a6afe83d3
Ofir-Roz <ofir19923@gmail.com>
2024-12-25 16:52:27 +0200

    Add todo reducer and combine it into the store file

--- store/store.js
+++ store/store.js
@@ -1,7 +1,12 @@
+import { todoReducer } from './reducers/todo.reducer.js';
 
 const { createStore, combineReducers, compose } = Redux
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = createStore(composeEnhancers())
+const rootReducer = combineReducers({
+    todoModule: todoReducer
+})
+
+export const store = createStore(rootReducer, composeEnhancers())
 window.gStore = store
